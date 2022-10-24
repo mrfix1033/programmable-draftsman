9bf8955f9dc4cad0d966b6d203f332f76066a192
Sergey <104345367+mrfix1033@users.noreply.github.com>
2022-10-24 22:17:06 +0300

    add comments

--- py.py
+++ py.py
@@ -2,9 +2,9 @@ import pygame
 
 
 class MyGame:
-    def __init__(self, WIDTH=852, HEIGHT=480, FPS=30, SCALE=1):
-        self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT
+    def __init__(self, WIDTH=852, HEIGHT=480, FPS=30, SCALE=1.5):
+        self.WIDTH = WIDTH * SCALE
+        self.HEIGHT = HEIGHT * SCALE
         self.FPS = FPS
         self.CENTER_X = WIDTH // 2
         self.CENTER_Y = HEIGHT // 2
@@ -37,15 +37,18 @@ class MyGame:
 
     def extraInit(self):
         self.drawObjects = []
+        # uncomment this code and see LoopedInt
         # self.x = LoopedInt(0, self.WIDTH - 1)
         # self.y = LoopedInt(0, self.HEIGHT - 1)
-        self.radius = 35
-        self.x = BounceInt(0 + self.radius, self.WIDTH - 1 - self.radius)
-        self.y = BounceInt(0 + self.radius, self.HEIGHT - 1 - self.radius)
+        self.radius = 35 * self.SCALE
+        # after comment previous code, uncomment this code and see BounceInt
+        # self.x = BounceInt(0 + self.radius, self.WIDTH - 1 - self.radius)
+        # self.y = BounceInt(0 + self.radius, self.HEIGHT - 1 - self.radius)
         self.counter_colors = BounceInt(0, 256 * 3 - 1)
+        # I want to add a gradient
 
     def extraDoTick(self):
-        self.handlePixels()
+        self.handleDrawObjects()
         self.x += 3
         self.y += 2
         self.counter_colors += 1
@@ -56,7 +59,7 @@ class MyGame:
                                            self.display.get_surface(), center=(int(self.x), int(self.y)),
                                            radius=self.radius, objectLiveTicks=150))
 
-    def handlePixels(self):
+    def handleDrawObjects(self):
         need_to_delete_indexes = []
         for i in range(len(self.drawObjects)):
             if self.drawObjects[i].doTick():
@@ -67,6 +70,9 @@ class MyGame:
 
 class DrawObject:
     def __init__(self, drawFunc, color, *args, objectLiveTicks=60, **kwargs):
+        # args, kwargs - arguments for drawFunc
+        # drawFunc - function that draws an object (circle, rectangle, pixel, etc.)
+        # if objectLiveTicks == -1, object isn't vanish
         self.drawFunc = drawFunc
         self.color = color
         self.args = args
@@ -85,12 +91,16 @@ class DrawObject:
         return self.remainedTicks == -1
 
     def multiplicateColor(self, color, multiplier):
+        # function that multiplies values of color
         out = []
         for i in color:
             out.append(round(i * multiplier))
         return out
 
 
+# start = 0, end = 10, num = 0
+# always +3
+# 0, 3, 6, 9, 2, 5, 8, 1, 4, 7, 10, 3, ...
 class LoopedInt:
     def __init__(self, start, end, num=0):
         self.shift = start
@@ -118,6 +128,9 @@ class LoopedInt:
         return self.num == other.num
 
 
+# start = 0, end = 10, num = 0
+# always +3
+# 0, 3, 6, 9, 8, 5, 2, 1, 4, 7, 10, 7, ...
 class BounceInt:
     def __init__(self, start, end, num=0):
         self.start = start
@@ -132,6 +145,7 @@ class BounceInt:
 
     def __add__(self, other):
         self.num += other * self.multiplier
+        # need to optimize
         while not self.start <= self.num <= self.end:
             if self.num < self.start:
                 self.num += self.start - self.num
